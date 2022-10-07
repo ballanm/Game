8f3ee140734cbfe3afab1a2642dc98c20f2e903c
ballanm <burtonlog20@gmail.com>
2022-10-07 11:11:56 -0600

    Basic start

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//while game loop in this file 
+//this will do everything and create objects
